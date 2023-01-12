7861a4c751f1eaa2b5fbe496e35789b48c7b1be7
prathmeshchhabra <p.chhabra2002@gmail.com>
2023-01-12 22:44:50 +0530

    First_Commit

---
+++ pages/new-meetup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+
+function NewMeetupPage() {
+    const addMeetupHandler = (enteredMeetupData) => {
+        console.log(enteredMeetupData);
+    }
+
+  return (
+    <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
+  )
+}
+
+export default NewMeetupPage
